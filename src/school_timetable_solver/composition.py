c5d70e7973d6be36399e98ba1484404214c899db
Ryogo Tsuchiya <70695192+RoyPvl@users.noreply.github.com>
2026-08-16 18:05:48 +0900

    Wire H23 successor-aware services

--- src/school_timetable_solver/composition.py
+++ src/school_timetable_solver/composition.py
@@ -7,6 +7,12 @@ from school_timetable_solver.adapter.excel_output_adapter import ExcelTimetableW
 from school_timetable_solver.adapter.execution_log_adapter import ExecutionLogAdapter
 from school_timetable_solver.constraint.hard_constraints import DEFAULT_HARD_CONSTRAINTS
 from school_timetable_solver.constraint.soft_constraints import DEFAULT_SOFT_CONSTRAINTS
+from school_timetable_solver.constraint.successor_constraints import (
+    ClassSuccessorConstraint,
+    SuccessorAwareRoomChangeGapPreferenceConstraint,
+    SuccessorAwareScheduleBalancePreferenceConstraint,
+    SuccessorAwareSingleLessonDayPreferenceConstraint,
+)
 from school_timetable_solver.service.generation_services import (
     GenerateTimetableService,
     ValidateInputService,
@@ -18,12 +24,25 @@ from school_timetable_solver.service.planning_services import (
 from school_timetable_solver.service.result_services import (
     AssignRoomsService,
     BuildTimetableDocumentService,
-    ValidateResultService,
 )
-from school_timetable_solver.service.solver_service import TimetableSolverService
-from school_timetable_solver.validator.input_validators import (
-    DEFAULT_INPUT_VALIDATORS,
-    CapacityFeasibilityValidator,
+from school_timetable_solver.service.successor_services import (
+    SuccessorCapacityFeasibilityValidator,
+    SuccessorTimetableSolverService,
+    SuccessorValidateResultService,
+)
+from school_timetable_solver.validator.input_validators import DEFAULT_INPUT_VALIDATORS
+
+
+SUCCESSOR_HARD_CONSTRAINTS = (*DEFAULT_HARD_CONSTRAINTS, ClassSuccessorConstraint())
+SUCCESSOR_SOFT_CONSTRAINTS = (
+    *(
+        constraint
+        for constraint in DEFAULT_SOFT_CONSTRAINTS
+        if constraint.rule_id not in {"S10", "S12", "S16"}
+    ),
+    SuccessorAwareRoomChangeGapPreferenceConstraint(),
+    SuccessorAwareSingleLessonDayPreferenceConstraint(),
+    SuccessorAwareScheduleBalancePreferenceConstraint(),
 )
 
 
@@ -40,7 +59,7 @@ class ApplicationComposition:
             validators=DEFAULT_INPUT_VALIDATORS,
             rule_resolver=RuleResolverService(),
             candidate_builder=CandidateBuilderService(),
-            capacity_validator=CapacityFeasibilityValidator(),
+            capacity_validator=SuccessorCapacityFeasibilityValidator(),
         )
 
     def create_generate_timetable_service(
@@ -53,13 +72,13 @@ class ApplicationComposition:
             validators=DEFAULT_INPUT_VALIDATORS,
             rule_resolver=RuleResolverService(),
             candidate_builder=CandidateBuilderService(),
-            capacity_validator=CapacityFeasibilityValidator(),
-            solver_service=TimetableSolverService(
-                DEFAULT_HARD_CONSTRAINTS,
-                DEFAULT_SOFT_CONSTRAINTS,
+            capacity_validator=SuccessorCapacityFeasibilityValidator(),
+            solver_service=SuccessorTimetableSolverService(
+                SUCCESSOR_HARD_CONSTRAINTS,
+                SUCCESSOR_SOFT_CONSTRAINTS,
             ),
             room_assigner=AssignRoomsService(),
-            result_validator=ValidateResultService(),
+            result_validator=SuccessorValidateResultService(),
             document_builder=BuildTimetableDocumentService(),
             output_writer=ExcelTimetableWriterAdapter(),
         )
