d8653a0dae97b19f4646addb9b571ee7a54bc990
Ryogo Tsuchiya <70695192+RoyPvl@users.noreply.github.com>
2026-08-28 00:39:52 +0900

    feat: wire project input loader into desktop editor

--- src/school_timetable_solver/desktop_composition.py
+++ src/school_timetable_solver/desktop_composition.py
@@ -13,6 +13,7 @@ from school_timetable_solver.service.project_services import (
     ExecuteProjectService,
     ImportProjectService,
     ListProjectsService,
+    LoadProjectInputService,
     LoadProjectService,
     UpdateProjectMetadataService,
 )
@@ -25,15 +26,14 @@ class DesktopApplicationComposition:
     def create_desktop_window(self, data_directory: Path) -> SeasonalDesktopWindow:
         project_store = LocalProjectStoreAdapter(data_directory)
         project_store.initialize()
+        input_reader = CompatibleExcelInputReaderAdapter()
         generator = ApplicationComposition().create_generate_timetable_service()
         return SeasonalDesktopWindow(
             list_projects=ListProjectsService(project_store),
             load_project=LoadProjectService(project_store),
+            load_project_input=LoadProjectInputService(project_store, input_reader),
             create_project=CreateProjectService(project_store),
-            import_project=ImportProjectService(
-                project_store,
-                CompatibleExcelInputReaderAdapter(),
-            ),
+            import_project=ImportProjectService(project_store, input_reader),
             update_project=UpdateProjectMetadataService(project_store),
             duplicate_project=DuplicateProjectService(project_store),
             delete_project=DeleteProjectService(project_store),
